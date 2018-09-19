c21ca12ff6d9a92f98bcd426ca109152c0f79c1e
Shalice Moideen <shalice.moideen@marlabs.com>
2018-09-19 15:18:24 +0530

    Index modified for debugging

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
 	import ipdb;ipdb.set_trace()
-    return HttpResponse("Hello, world. You're at the core index.")
+	return HttpResponse("Hello, world. You're at the core index.")
 
 def create_meetings(request):
   from selenium import webdriver
